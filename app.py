6992111a87d6ba4e4ba6f126e359b3615052e8c6
ilmawf <ilmafwafa@gmail.com>
2026-01-19 16:54:58 +0530

    Added cozy homepage template and sad emotion response

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def home():
-    return "CalmCheck is running 🌱"
+    return render_template("index.html")
+
+@app.route("/feel", methods=["POST"])
+def feel():
+    emotion = request.form.get("emotion")
+
+    if emotion == "sad":
+        message = "Thank you for sharing that. You don’t have to carry it alone."
+
+    return render_template("index.html", response=message)
 
 if __name__ == "__main__":
     app.run(debug=True)
